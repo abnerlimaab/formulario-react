802ab863f9d86b4c8fde1704b93fc2b0188c723d
Abner.Lima <abnerlima.1997@gmail.com>
2022-02-19 15:46:29 -0300

    Criando novos formulários

---
+++ src/components/FormularioCadastro/DadosUsuario.jsx
@@ -0,0 +1,33 @@
+import { Button, TextField } from '@mui/material';
+import React from 'react';
+
+function DadosUsuario() {
+    return (
+        <form>
+            <TextField
+                id='email'
+                label='email'
+                type='email'
+                variant="outlined" 
+                fullWidth 
+                margin='normal'
+            />
+            <TextField
+                id='senha'
+                label='senha'
+                type='password'
+                variant="outlined" 
+                fullWidth 
+                margin='normal'
+            />
+            <Button
+                type='submit' 
+                variant='contained' 
+                color='primary'
+            >Cadastrar
+            </Button>
+        </form>
+    );
+}
+
+export default DadosUsuario;
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,91 +1,18 @@
-import React, {useState} from 'react';
-import { Switch, Button, TextField, FormControlLabel } from '@mui/material';
+import React from 'react';
+import DadosEntrega from './DadosEntrega';
+import DadosPessoais from './DadosPessoais';
+import DadosUsuario from './DadosUsuario';
 
 function FormularioCadastro({aoEnviar, validarCPF}) {
-        const [nome, setNome] = useState("");
-        const [sobrenome, setSobrenome] = useState("");
-        const [cpf, setCPF] = useState("");
-        const [promocoes, setPromocoes] = useState(true);
-        const [novidades, setNovidades] = useState(true);
-        const [erros, setErros] = useState({cpf: {valido: true, texto: ""}});
     return(
-        <form
-            onSubmit={e => {
-                e.preventDefault();
-                aoEnviar({nome, sobrenome, cpf, promocoes, novidades});
-            }}
-        >
-            <TextField 
-                value={nome}
-                onChange={
-                    e => setNome(e.target.value)
-                }
-                id="nome" 
-                label="Nome" 
-                variant="outlined" 
-                fullWidth 
-                margin='normal'
+        <>
+            <DadosPessoais
+                aoEnviar={aoEnviar}
+                validarCPF={validarCPF}
             />
-            <TextField 
-                value={sobrenome}
-                onChange={
-                    e => setSobrenome(e.target.value)
-                }
-                id="sobrenome" 
-                label="Sobrenome" 
-                variant="outlined" 
-                fullWidth 
-                margin='normal'
-            />
-            <TextField 
-                value={cpf}
-                onChange={
-                    e => setCPF(e.target.value)
-                }
-                onBlur={
-                    e => {
-                        const ehValido = validarCPF(cpf);
-                        setErros({cpf: ehValido});
-                    }
-                }
-                error={!erros.cpf.valido}
-                helperText={erros.cpf.texto}
-                id="CPF" 
-                label="CPF" 
-                variant="outlined" 
-                fullWidth 
-                margin='normal'
-            />
-            <FormControlLabel 
-                control={
-                    <Switch 
-                        checked={promocoes}
-                        onChange={
-                            e => setPromocoes(e.target.checked)
-                        }
-                        name="promocoes" 
-                    />
-                }
-                label="Promoções"
-            />
-            <FormControlLabel 
-                control={
-                    <Switch 
-                        checked={novidades}
-                        onChange={
-                            e => setNovidades(e.target.checked)
-                        }
-                        name="novidades" 
-                    />
-                }
-                label="Novidades"
-            />
-            <Button 
-                type='submit' 
-                variant='contained' 
-                color='primary'
-            >Cadastrar</Button>
-        </form>
+            <DadosUsuario/>
+            <DadosEntrega/>
+        </>
     )
 }
 
